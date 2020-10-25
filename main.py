f8e89347a73efccdbe9553d01256828515a0fe30
HIMADRI SEKHAR BASU <hsb10@iitbbs.ac.in>
2020-10-25 15:12:37 +0530

    Updated tabs in main.py

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ datafile = open("phase1000000.plt", "rb")
 
 #class read_tecplot_data(datafile):
 def test_read_data(data):
-    str=data.read()
-    headerr = pyPLTreader.read_header(str)
-    read_datafile = pyPLTreader.read_data(str, headerr, data)
-    return read_datafile
+	'''Returned argument a is squared.'''
+	str=data.read()
+	headerr = pyPLTreader.read_header(str)
+	read_datafile = pyPLTreader.read_data(str, headerr, data)
+	return read_datafile
 
 read_data = test_read_data(datafile)
 print('\n========================================\n')
@@ -19,10 +20,10 @@ data_dict = read_data['Zones'][0]
 
 print(vars_list)
 for i in range(0,len(vars_list)):
-    var_name = vars_list[i]
-    var_data = data_dict[vars_list[i]]
-
-    print('\n========================================\n')
-    print(type(var_data))
-    print(var_name, '=', var_data)
-    print('\n========================================\n')   
+	var_name = vars_list[i]
+	var_data = data_dict[vars_list[i]]
+	
+	print('\n========================================\n')
+	print(type(var_data))
+	print(var_name, '=', var_data)
+	print('\n========================================\n')   
